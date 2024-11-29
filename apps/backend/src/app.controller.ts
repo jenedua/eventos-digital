d5e6d6255bfb683169d8629fa4f092bdbb0ef432
Fedner <91825533+jenedua@users.noreply.github.com>
2024-11-29 19:06:13 -0300

    Adicionando backend como parte do repositório principal

---
+++ apps/backend/src/app.controller.ts
@@ -0,0 +1,12 @@
+/* eslint-disable prettier/prettier */
+import { Controller, Get } from '@nestjs/common';
+
+@Controller()
+export class AppController {
+  constructor() {}
+
+  @Get('/ping')
+  ping(): string {
+    return "pong";
+  }
+}
